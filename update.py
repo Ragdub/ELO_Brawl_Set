cd408ce18c72694e1e02f11a406e81da43e49c8d
Dylan Bellier <dylan.bellier@ens-rennes.fr>
2025-07-13 15:55:48 +0200

    update is now made of many subfunctions

--- update.py
+++ update.py
@@ -30,6 +30,176 @@ def getEloModifier(won, elo_player, elo_opponent):
     """
     return won - 1 / ( 1 + 10 ** ((elo_opponent - elo_player) / 400))
 
+def readPlayer(player, players_global, data_changed, ELO_clearance, elo_clearances, is_trusted):
+    """Returns the json of the player "player" in players_global and stores whether the player have agreed to have a personal ELO in elo_clearance. If player is not in players_global, returns a new entry and adds "players" to data_changed.
+    
+    player: the name of the player.
+    players_global: the data base of all players data.
+    data_changed: tracks new entries in the data_base.
+    ELO_clearance: list of players that have agreed to have a personal ELO.
+    elo_clearances: list of clearances.
+    is_trusted: bypass input verification.
+    """
+    elo_clearance = player in ELO_clearance
+    elo_clearances.append(elo_clearance)
+    try:
+        return players_global[player]
+    except KeyError:
+        is_player_correct = "Y"
+        if not is_trusted:
+            is_player_correct = input(f"Is {player} a new player? (Y/n)")
+        if is_player_correct == "Y" or is_player_correct == "y" or is_player_correct == "" or is_trusted:
+            if elo_clearance:
+                new_player = getDefaultPlayerELO()
+            else:
+                new_player = getDefaultPlayerNoELO()
+            players_global[player] = new_player
+            data_changed.add("players")
+            return new_player
+        else:
+            raise Exception("Bad player name")
+
+def readSet(magic_set, decks_global, data_changed, is_trusted):
+    """Returns the json of the set "magic_set" in decks_global. If magic_set is not in decks_global, returns a new entry and adds "sets" to data_changed.
+    
+    magic_set: the name of the set.
+    decks_global: the data base of all decks data, organized in sets.
+    data_changed: tracks new entries in the data_base.
+    is_trusted: bypass input verification.
+    """
+    try:
+        return decks_global[magic_set]
+    except KeyError:
+        is_set_correct = "Y"
+        if not is_trusted:
+            is_set_correct = input(f"Is {magic_set} a correct magic set? (Y/n)")
+        magic_set_work = dict()
+        if is_set_correct == "Y" or is_set_correct == "y" or is_set_correct == "" or is_trusted:
+            decks_global[magic_set] = magic_set_work
+            data_changed.add("sets")
+            return magic_set_work
+        else:
+            raise Exception("Bad set name")
+
+def readDeck(deck, decks_global, magic_set_work, data_changed, is_trusted):
+    """Returns the json of the deck "deck" in decks_global[magic_set_work]. If deck is not in decks_global[magic_set_work], returns a new entry and adds "decks" to data_changed.
+    
+    deck: the name of the deck.
+    decks_global: the data base of all decks data, organized in sets.
+    magic_set_work: the name of the set of "deck"
+    data_changed: tracks new entries in the data_base.
+    is_trusted: bypass input verification.
+    """
+    try:
+        return magic_set_work[deck]
+    except KeyError:
+        is_deck_correct = "Y"
+        if not is_trusted:
+            is_deck_correct = input(f"Is {deck} a correct deck from the set {magic_set}? (Y/n)")
+        if is_deck_correct == "Y" or is_deck_correct == "y" or is_deck_correct == "" or is_trusted:
+            new_deck = getDefaultDeck()
+            magic_set_work[deck] = new_deck
+            data_changed.add("decks")
+            return new_deck
+        else:
+            raise Exception("Bad deck name")
+
+def computeELOmixte(decks, players, data_label, elo_clearances):
+    """Returns a list of the mixed elo.
+    """
+    elo_mixed = []
+    for index in range(2):
+        deck_elo = decks[index][data_label]["ELO"]
+        if elo_clearances[index]:
+            player_elo = players[index][data_label]["ELO"]
+        else:
+            player_elo = deck_elo
+        elo_mixed.append(ALPHA * deck_elo + (1 - ALPHA) * player_elo)
+    return elo_mixed
+
+def computeELOmodifier(scores, elo_mixed):
+    """Returns a list of the ELO modifiers.
+    """
+    elo_modifier = []
+    for index in range(2):
+        opp_index = 1 - index
+        if scores[index] > scores[opp_index]:
+            won = 1.
+        elif scores[index] < scores[opp_index]:
+            won = 0.
+        else:
+            won = .5
+        elo_modifier.append(getEloModifier(won, elo_mixed[index], elo_mixed[opp_index]))
+    return elo_modifier
+
+def computeResult(scores, index):
+    """Return the result given the scores.
+    """
+    opp_index = 1 - index
+    if scores[index] > scores[opp_index]:
+        return "win"
+    elif scores[index] < scores[opp_index]:
+        return "lose"
+    else:
+        return "draw"
+
+def updateDeck(deck, opponent_deck_name, player_name, date, result, elo_modifier):
+    """Updates the data of the deck "deck".
+    """
+    new_deck_elo = deck["ELO"] + K_DECKS * elo_modifier
+    deck["ELO"] = new_deck_elo
+    deck["historic"][date] = new_deck_elo
+    deck["match number"] += 1
+    if not player_name in deck["players"].keys():
+        deck["players"][player_name] = 0
+    deck["players"][player_name] += 1
+    if not opponent_deck_name in deck["decks fought"].keys():
+        deck["decks fought"][opponent_deck_name] = {"win" : 0, "draw" : 0, "lose" : 0}
+    if result == "win":
+        deck["decks fought"][opponent_deck_name]["win"] += 1
+    elif result == "lose":
+        deck["decks fought"][opponent_deck_name]["lose"] += 1
+    else:
+        deck["decks fought"][opponent_deck_name]["draw"] += 1
+
+def updatePlayer(player, elo_clearance, opponent_name, deck_name, date, result, elo_modifier):
+    """Updates the data of the player "player".
+    """
+    if elo_clearance:
+        new_player_elo = player["ELO"] + K_PLAYER * elo_modifier
+        player["ELO"] = new_player_elo
+        player["historic"][date] = new_player_elo
+    player["match number"] += 1
+    if not deck_name in player["decks played"].keys():
+        player["decks played"][deck_name] = 0
+    player["decks played"][deck_name] += 1
+    if not opponent_name in player["players fought"].keys():
+        player["players fought"][opponent_name] = {"win" : 0, "draw" : 0, "lose" : 0}
+    if result == "win":
+        player["players fought"][opponent_name]["win"] += 1
+    elif result == "lose":
+        player["players fought"][opponent_name]["lose"] += 1
+    else:
+        player["players fought"][opponent_name]["draw"] += 1
+
+def updateData(decks, players, decks_names, players_names, scores, date, elo_clearances, data_label):
+    """Updates the data in decks and players.
+    """
+    elo_mixed = computeELOmixte(decks, players, data_label, elo_clearances)
+    elo_modifier = computeELOmodifier(scores, elo_mixed)
+    for index in range(2):
+        opp_index = 1 - index
+        result = computeResult(scores, index)
+        deck = decks[index][data_label]
+        player = players[index][data_label]
+        player_name = players_names[index]
+        deck_name = decks_names[index]
+        opponent_name = players_names[opp_index]
+        opponent_deck_name = decks_names[opp_index]
+        
+        updateDeck(deck, opponent_deck_name, player_name, date, result, elo_modifier[index])
+        updatePlayer(player, elo_clearances[index], opponent_name, deck_name, date, result, elo_modifier[index])
+
 def processRencontre(rencontre, players_global, ELO_clearance, decks_global, is_trusted):
     """Process a match and update the ELO of decks and players involved
     Return which among "decks", "players" and "magic_sets" has a new entry
@@ -44,125 +214,27 @@ def processRencontre(rencontre, players_global, ELO_clearance, decks_global, is_
     decks = []
     decks_names = []
     scores = []
+    date = rencontre["Date"]
+    elo_clearances = []
     for suffix in ["A", "B"]:
         player = rencontre[f"Joueureuse{suffix}"]
         players_names.append(player)
         deck = rencontre[f"Deck{suffix}"]
         decks_names.append(deck)
         magic_set = rencontre[f"Set{suffix}"]
-        try:
-            players.append(players_global[player])
-        except KeyError:
-            is_player_correct = "Y"
-            if not is_trusted:
-                is_player_correct = input(f"Is {player} a new player? (Y/n)")
-            if is_player_correct == "Y" or is_player_correct == "y" or is_player_correct == "" or is_trusted:
-                if player in ELO_clearance:
-                    new_player = getDefaultPlayerELO()
-                else:
-                    new_player = getDefaultPlayerNoELO()
-                players_global[player] = new_player
-                players.append(new_player)
-                data_changed.add("players")
-            else:
-                raise Exception("Bad player name")
-        try:
-            magic_set_work = decks_global[magic_set]
-        except KeyError:
-            is_set_correct = "Y"
-            if not is_trusted:
-                is_set_correct = input(f"Is {magic_set} a correct magic set? (Y/n)")
-            magic_set_work = dict()
-            if is_set_correct == "Y" or is_set_correct == "y" or is_set_correct == "" or is_trusted:
-                decks_global[magic_set] = magic_set_work
-                data_changed.add("sets")
-            else:
-                raise Exception("Bad set name")
-        try:
-            decks.append(magic_set_work[deck])
-        except KeyError:
-            is_deck_correct = "Y"
-            if not is_trusted:
-                is_deck_correct = input(f"Is {deck} a correct deck from the set {magic_set}? (Y/n)")
-            if is_deck_correct == "Y" or is_deck_correct == "y" or is_deck_correct == "" or is_trusted:
-                new_deck = getDefaultDeck()
-                magic_set_work[deck] = new_deck
-                decks.append(new_deck)
-                data_changed.add("decks")
-            else:
-                raise Exception("Bad deck name")
         scores.append(rencontre[f"Score{suffix}"])
+        
+        players.append(readPlayer(player, players_global, data_changed, ELO_clearance, elo_clearances, is_trusted))
+        magic_set_work = readSet(magic_set, decks_global, data_changed, is_trusted)
+        decks.append(readDeck(deck, decks_global, magic_set_work, data_changed, is_trusted))
 
     data_to_modify = [ "Data" ]
     if rencontre["Tournois"]:
         data_to_modify.append("Data Tournoi")
 
     for data_label in data_to_modify:
-        elo_mixed = []
-        for index in range(2):
-            deck_elo = decks[index][data_label]["ELO"]
-            if players_names[index] in ELO_clearance:
-                player_elo = players[index][data_label]["ELO"]
-            else:
-                player_elo = deck_elo
-            elo_mixed.append(ALPHA * deck_elo + (1 - ALPHA) * player_elo)
-
-        elo_modifier = []
-        for index in range(2):
-            opp_index = 1 - index
-            if scores[index] > scores[opp_index]:
-                won = 1.
-            elif scores[index] < scores[opp_index]:
-                won = 0.
-            else:
-                won = .5
-            elo_modifier.append(getEloModifier(won, elo_mixed[index], elo_mixed[opp_index]))
-
-        # Modifying elos
-        for index in range(2):
-            opp_index = 1 - index
-            deck = decks[index][data_label]
-            player = players[index][data_label]
-            player_name = players_names[index]
-            deck_name = decks_names[index]
-            new_deck_elo = deck["ELO"] + K_DECKS * elo_modifier[index]
-            if player_name in ELO_clearance:
-                new_player_elo = player["ELO"] + K_PLAYER * elo_modifier[index]
-            opponent_name = players_names[opp_index]
-            opponent_deck_name = decks_names[opp_index]
-            
-            # decks and player are storing references to the data in the global datas.
-            # we first update the data of the deck
-            deck["ELO"] = new_deck_elo
-            deck["match number"] += 1
-            if not player_name in deck["players"].keys():
-                deck["players"][player_name] = 0
-            deck["players"][player_name] += 1
-            if not opponent_deck_name in deck["decks fought"].keys():
-                deck["decks fought"][opponent_deck_name] = {"win" : 0, "draw" : 0, "lose" : 0}
-            if scores[index] > scores[opp_index]:
-                deck["decks fought"][opponent_deck_name]["win"] += 1
-            elif scores[index] < scores[opp_index]:
-                deck["decks fought"][opponent_deck_name]["lose"] += 1
-            else:
-                deck["decks fought"][opponent_deck_name]["draw"] += 1
-            deck["historic"][rencontre["Date"]] = new_deck_elo
-            # then we update the data of the player
-            if player_name in ELO_clearance:
-                player["ELO"] = new_player_elo
-                player["historic"][rencontre["Date"]] = new_player_elo
-            player["match number"] += 1
-            if not deck_name in player["decks played"].keys():
-                player["decks played"][deck_name] = 0
-            if not opponent_name in player["players fought"].keys():
-                player["players fought"][opponent_name] = {"win" : 0, "draw" : 0, "lose" : 0}
-            if scores[index] > scores[opp_index]:
-                player["players fought"][opponent_name]["win"] += 1
-            elif scores[index] < scores[opp_index]:
-                player["players fought"][opponent_name]["lose"] += 1
-            else:
-                player["players fought"][opponent_name]["draw"] += 1
-            player["decks played"][deck_name] += 1
+        updateData(decks, players, decks_names, players_names, scores, date, elo_clearances, data_label)
+    
     return data_changed
 
 if __name__ == "__main__" :
